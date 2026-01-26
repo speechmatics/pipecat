08dbfcdb1883ab55ff1fada427f888126dd2dee2
Sam Sykes <sams@speechmatics.com>
2026-01-26 17:00:45 +0000

    Squashed commit of the following:
    commit fc1444c9d628fbcbc492bde1e4642c666ba926b4
    Author: Sam Sykes <sams@speechmatics.com>
    Date:   Mon Jan 26 16:25:37 2026 +0000
    
        Updated changelog
    
    commit ea94939add121c54039c1cba07314a686a959a78
    Author: Sam Sykes <sams@speechmatics.com>
    Date:   Mon Jan 26 16:24:56 2026 +0000
    
        update dependency
    
    commit 0c69ae63710d426b3d372dd7a6b5868c0c689ae5
    Author: Sam Sykes <sams@speechmatics.com>
    Date:   Mon Jan 26 16:07:59 2026 +0000
    
        Changelog entry.
    
    commit 8b88280bb10a5bc1fb22e00fe39f358669df25b2
    Author: Sam Sykes <sams@speechmatics.com>
    Date:   Mon Jan 26 15:52:42 2026 +0000
    
        Default to using `EXTERNAL` mode.
    
    commit 960d0faea5f39ff2d2d0ede3f6d28950bb783ba0
    Author: Sam Sykes <sams@speechmatics.com>
    Date:   Mon Jan 26 15:48:04 2026 +0000
    
        support `is_eou` for final segment in utterance

--- src/pipecat/services/speechmatics/stt.py
+++ src/pipecat/services/speechmatics/stt.py
@@ -66,7 +66,7 @@ class TurnDetectionMode(str, Enum):
     """Endpoint and turn detection handling mode.
 
     How the STT engine handles the endpointing of speech. If using Pipecat's built-in endpointing,
-    then use `TurnDetectionMode.FIXED` (default).
+    then use `TurnDetectionMode.EXTERNAL` (default).
 
     To use the STT engine's built-in endpointing, then use `TurnDetectionMode.ADAPTIVE` for simple
     voice activity detection or `TurnDetectionMode.SMART_TURN` for more advanced ML-based
@@ -106,7 +106,7 @@ class SpeechmaticsSTTService(STTService):
 
             turn_detection_mode: Endpoint handling, one of `TurnDetectionMode.FIXED`,
                 `TurnDetectionMode.EXTERNAL`, `TurnDetectionMode.ADAPTIVE` and
-                `TurnDetectionMode.SMART_TURN`. Defaults to `TurnDetectionMode.FIXED`.
+                `TurnDetectionMode.SMART_TURN`. Defaults to `TurnDetectionMode.EXTERNAL`.
 
             speaker_active_format: Formatter for active speaker ID. This formatter is used to format
                 the text output for individual speakers and ensures that the context is clear for
@@ -207,7 +207,7 @@ class SpeechmaticsSTTService(STTService):
         language: Language | str = Language.EN
 
         # Endpointing mode
-        turn_detection_mode: TurnDetectionMode = TurnDetectionMode.FIXED
+        turn_detection_mode: TurnDetectionMode = TurnDetectionMode.EXTERNAL
 
         # Output formatting
         speaker_active_format: str | None = None
@@ -734,9 +734,16 @@ class SpeechmaticsSTTService(STTService):
 
         # If final, then re-parse into TranscriptionFrame
         if finalized:
-            frames += [TranscriptionFrame(**attr_from_segment(segment)) for segment in segments]
+            frames += [
+                TranscriptionFrame(
+                    **attr_from_segment(segment), finalized=segment.get("is_eou", False)
+                )
+                for segment in segments
+            ]
             finalized_text = "|".join([s["text"] for s in segments])
-            await self._handle_transcription(finalized_text, True, segments[0]["language"])
+            await self._handle_transcription(
+                finalized_text, is_final=True, language=segments[0]["language"]
+            )
             logger.debug(f"{self} finalized transcript: {[f.text for f in frames]}")
 
         # Return as interim results (unformatted)
